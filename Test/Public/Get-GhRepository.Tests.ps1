0d4e58057bf7590c65f0f8c275a574d4f16c9feb
Tyler Vaughan <tyler.vaughan@pnnl.gov>
2023-12-05 10:59:24 -0800

    Working mock

--- Test/Public/Get-GhRepository.Tests.ps1
+++ Test/Public/Get-GhRepository.Tests.ps1
@@ -1,11 +1,8 @@
 BeforeAll {
-    . 'C:\Users\gamers\Documents\Tyler\GHCICD\test-and-analyze-code-lp-author\Source\Private\Invoke-Gh.ps1'
     . $PSCommandPath.Replace('.Tests.ps1', '.ps1').Replace('\Test\', '\Source\')
 }
 Describe "Get-GhRepository Tests" {
-    BeforeAll {
-        Mock Invoke-Gh ($endpoint) {}
-    }
+
     Context "Mandatory parameters" {
         It "Owner" {
             Get-Command Get-GhRepository | Should -HaveParameter Owner -Mandatory -Type String
@@ -15,6 +12,10 @@ Describe "Get-GhRepository Tests" {
         }
     }
     Context "Confirm correct URL" {
+        BeforeAll {
+            function Invoke-Gh ($endpoint) {}
+            Mock Invoke-Gh -ParameterFilter {$endpoint} -MockWith {}
+        }
         It "API call" {
             $owner = "Tyler"
             $repo = "MyOwn"
